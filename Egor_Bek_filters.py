f9df6e222caff30a0c22df782985113134f46f28
Egor <egorka.bekrenev@mail.ru>
2023-11-26 22:00:42 +0900

    init commit

--- Egor_Bek_filters.py
+++ Egor_Bek_filters.py
@@ -108,26 +108,3 @@ class bw_reverse_Filter():
         transformed_pixel_values = [255 - value for value in pixel_values]
         img.putdata(transformed_pixel_values)
         img.save(name)
-
-class bitie_pixeli_Filter():
-    def __init__(self, image):
-        self.image = image
-
-    def get_filter(self):
-        img = Image.open(self.image)
-        for x in range(img.width, randint(0, 100)):
-            for y in range(img.height, randint(0, 100)):
-                pixel = img.getpixel(x, y)
-                print(pixel)
-        img.show()
-
-    def img_save(self, name):
-        img = Image.open(self.image)
-        for y in range(img.height):
-            for x in range(img.width):
-                r, g, b = img.getpixel((x, y))
-                r = min(255, int(r * 0))
-                g = min(255, int(g * 0))
-                b = min(255, int(b * 1))
-                img.putpixel((x, y), (r, g, b))
-        img.save(name)
